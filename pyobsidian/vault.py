d975142fa60f51d7ccd4ca7975c008582d32082d
matheussrod <matheus.srodr@gmail.com>
2024-06-03 22:48:04 -0300

    feat: add adder in vault execute

--- pyobsidian/vault.py
+++ pyobsidian/vault.py
@@ -6,12 +6,21 @@ A vault is a collection of notes that can be searched through.
 
 from __future__ import annotations
 
-from pyobsidian.filter import Field, FieldKey, FieldValue, FieldOcurrence
-from pyobsidian.filter import Filter, FilterField, FilterMode
+from pyobsidian.adder import (
+    Adder, AdderWhere, AdderField,
+    AdderKey, AdderFieldWhere, AdderValue
+)
+from pyobsidian.adder_op import Op
+from pyobsidian.filter import (
+    Field, FieldKey, FieldOcurrence, FieldValue,
+    Filter, FilterField, FilterMode
+)
 from pyobsidian.note import Note
 from pyobsidian.searchby import get_search_strategies, SearchBy
+from pyobsidian.formatter import Formatter
 import os
-from typing import Self
+from typing import Literal, Self
+import textwrap
 
 class Vault:
     """A class that represents an Obsidian vault.
@@ -26,16 +35,19 @@ class Vault:
         The filter applied to the vault
     """
     __search_strategies = get_search_strategies()
+    __adder_strategies = Adder()
 
     def __init__(
         self: Self,
         path: str,
         notes: list[Note] = [],
-        filter: Filter = Filter()
+        filter: Filter = Filter(),
+        adder: Adder = Adder()
     ) -> None:
         self.__path = path
         self.__notes = notes
         self.__filter = filter
+        self.__adder = adder
 
     @property
     def path(self: Self) -> str:
@@ -68,6 +80,16 @@ class Vault:
         Filter
             The filter applied to the vault"""
         return self.__filter
+    
+    @property
+    def adder(self: Self) -> Adder:
+        """The adder applied to the vault
+        
+        Returns
+        -------
+        Filter
+            The adder applied to the vault"""
+        return self.__adder
 
     @property
     def search_strategies(self: Self) -> dict[FieldKey, SearchBy]:
@@ -79,6 +101,28 @@ class Vault:
             The search strategies used by the vault
         """
         return self.__search_strategies
+
+    @property
+    def adder_strategies(self: Self) -> dict[AdderKey, AdderWhere]:
+        """The adder strategies used by the vault
+        
+        Returns
+        -------
+        dict[AdderKey, AdderWhere]
+            The adder strategies used by the vault
+        """
+        return self.__adder_strategies.registry.where
+
+    @property
+    def formatters(self: Self) -> dict[tuple[str, str], Formatter]:
+        """The formatters used by the vault
+        
+        Returns
+        -------
+        dict[tuple[str, str], Formatter]
+            The formatters used by the vault
+        """
+        return self.__adder_strategies.registry.formatters
 
     @classmethod
     def add_new_search_strategy(
@@ -103,6 +147,53 @@ class Vault:
         cls.__search_strategies[name] = strategy
         new_strategy = cls.__search_strategies
         return new_strategy
+
+    @classmethod
+    def add_new_adder_strategy(
+        cls: type[Self],
+        name: AdderKey,
+        strategy: AdderWhere
+    ) -> dict[str, AdderWhere]:
+        """Adds a new adder strategy to Vault class.
+
+        Parameters
+        ----------
+        name : AdderKey
+            The name of the new adder strategy.
+        strategy : AdderField
+            The adder strategy to be added.
+
+        Returns
+        -------
+        dict[str, AdderWhere]
+            The updated adder strategies.
+        """
+        cls.__adder_strategies.registry.add_registry(name, strategy)
+        return cls.__adder_strategies.registry.where
+
+    @classmethod
+    def add_new_formatter(
+        cls: type[Self],
+        name: str,
+        where: str,
+        formatter: Formatter
+    ) -> dict[tuple[str, str], Formatter]:
+        """Adds a new adder formatter to Vault class.
+
+        Parameters
+        ----------
+        name : AdderKey
+            The name of the new adder formatter.
+        formatter : AdderField
+            The formatter to be added.
+
+        Returns
+        -------
+        dict[tuple[str, str], Formatter]
+            The updated adder formatters.
+        """
+        cls.__adder_strategies.registry.add_formatter(name, where, formatter)
+        return cls.__adder_strategies.registry.formatters
 
     def get_notes(self: Self, extension: str='.md') -> list[Note]:
         """Retrieves a list of notes from the specified vault directory.
@@ -172,6 +263,43 @@ class Vault:
         new_vault = Vault(path=self.path, notes=self.notes, filter=new_filter)
         return new_vault
     
+    def add(
+        self: Self, 
+        by: AdderKey, 
+        value: AdderValue,
+        where: Literal['inline', 'yaml'] | Op
+    ) -> Vault:
+        """Add a new adder field to current adder.
+
+        `by`, `value` and `where` build the field to add.
+        
+        - by is used to find which AdderWhere to add to.
+        - value is the value to add.
+        - where is the where to add the value: inline, yaml or an Op.
+
+        Parameters
+        ----------
+        by : AdderKey
+            The field to add by.
+        value : AdderValue
+            The value to add.
+        where : Literal['inline', 'yaml'] | Op
+            The where to add the field.
+        
+        Returns
+        -------
+        Vault
+            A new Vault object with the new adder field added.
+        """
+        if isinstance(where, Op):
+            adder_field = AdderField(by, value, (where.id, where))
+        else:
+            adder_field = AdderField(by, value, where)
+        cur_adder = self.adder
+        new_adder = cur_adder.add_field(adder_field)
+        new_vault = Vault(path=self.path, notes=self.notes, filter=self.filter, adder=new_adder)
+        return new_vault
+
     def __search(
         self,
         notes: list[Note], 
@@ -196,7 +324,9 @@ class Vault:
         return notes
     
     def execute(self: Self) -> Vault:        
-        """This method applies the filter to the current Vault object.
+        """This method applies the filter and adder to the current Vault object.
+
+        First, all filter fields are applied. Then, all adder fields are applied.
 
         Returns
         -------
@@ -221,10 +351,51 @@ class Vault:
             else:
                 raise ValueError(f'`{mode}` is not a valid mode. Must be `and` or `or`')
         unique_new_notes = list(set(new_notes))
-        new_vault = Vault(path=self.path, notes=unique_new_notes, filter=filter)
-        return new_vault
+
+        adder = self.adder
+        if not adder:
+            return Vault(path=self.path, notes=unique_new_notes, filter=filter, adder=Adder())
+        for note in unique_new_notes:
+            for add in adder:
+                key = add.key
+                value = add.value
+                where = add.where
+                formatter = adder.registry.get_formatter(key, where)
+                adder_where = adder.registry.get_where(where)
+                if adder_where is None:
+                    message = textwrap.dedent(f"""
+                        Invalid where: '{where}' in `.add('{key}', {value}, '{where}')`
+                        Checking in {adder.registry.where} I found {list(adder.registry.where.keys())}.
+                        Hints:
+                            - Check that you haven't made any typing errors.
+                            - Check if AdderWhere implementation exists.
+                            - If you don't find it, add new adder strategy.
+                                - To do this, implement your own AdderWhere.
+                                - Use Vault.add_new_adder_strategy()
+                    """)
+                    raise ValueError(message)
+                if formatter is None:
+                    message = textwrap.dedent(f"""
+                        Invalid formatter: "{(key, where)}" in `.add('{key}', {value}, '{where}')`
+                        Checking in {adder.registry.formatters} I found {list(adder.registry.formatters.keys())}.
+                        I need a Formatter to format the value because this operation will modify the content of the note.
+                        In your case, you need to implement at least a Formatter for '{key}' with '{where}'.
+                        Hints:
+                            - Check that you haven't made any typing errors.
+                            - Check if Formatter implementation exists.
+                            - If you don't find it, add new formatter.
+                                - To do this, implement your own Formatter. 
+                                - Use Vault.add_new_adder_formatter()
+                    """)
+                    raise ValueError(message)
+                new_value = formatter.format(value)
+                new_field = AdderField(key, new_value, where)
+                new_content = adder_where.exec(note, new_field)
+                if new_content is None:
+                    continue
+                note.properties.content = new_content
+            note.write()
+        return Vault(path=self.path, notes=unique_new_notes, filter=filter, adder=Adder())
     
     def __repr__(self) -> str:
-        return f"Vault('{self.path}', {self.notes}, {self.filter})"
-
-    
+        return f"Vault('{self.path}', {self.notes}, {self.filter}, {self.adder})"
